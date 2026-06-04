5560bc5e7dc1b02ce5b8cb22c61776b598b7be88
Grigor Grigoryan <grigorhello@gmail.com>
2026-06-03 20:42:44 -0400

    Add transaction domain model

--- ScotiaAssessment/Models/Transaction.swift
+++ ScotiaAssessment/Models/Transaction.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Transaction: Identifiable, Equatable {
+    let id: String
+    let type: TransactionType
+    let merchantName: String
+    let description: String?
+    let amount: Decimal
+    let currency: String
+    let postedDate: String
+    let fromAccount: String
+    let fromCardNumber: String
+}
--- ScotiaAssessment/Models/TransactionType.swift
+++ ScotiaAssessment/Models/TransactionType.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+enum TransactionType: Equatable {
+    case credit
+    case debit
+}
