831abf5949a41459eba9ab12822317d2426390de
Grigor Grigoryan <grigorhello@gmail.com>
2026-06-04 12:09:22 -0400

    Add TransactionListState  and fetch transaction from service

--- ScotiaAssessment/ViewModels/TransactionListViewModel.swift
+++ ScotiaAssessment/ViewModels/TransactionListViewModel.swift
@@ -14,13 +14,33 @@ final class TransactionListViewModel {
 
     // MARK: Properties
 
+    private(set) var transactions: [Transaction] = []
+    private(set) var state: TransactionListState = .idle
+    private let transactionService: TransactionService
 
     // MARK: Initialization
 
-    init() {
-
+    init(transactionService: TransactionService) {
+        self.transactionService = transactionService
     }
 
     // MARK: Functions
+
+    func fetchTransactions() async {
+        state = .loading
+        do {
+            transactions = try await transactionService.fetchTransactions()
+            state = .success
+        } catch {
+            state = .error(error.localizedDescription)
+        }
+    }
 }
 
+
+enum TransactionListState {
+    case idle      // The view has just appeared and no action has been taken yet
+    case loading   // The API call is actively running
+    case success   // Data loaded successfully
+    case error(String)   // An error occurred
+}
