827a7032ec26d9ca773c95f50c45864201b9ab49
Grigor Grigoryan <grigorhello@gmail.com>
2026-06-03 21:38:41 -0400

    Add empty transaction list view and view model

---
+++ ScotiaAssessment/ViewModels/TransactionListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  TransactionListViewModel.swift
+//  ScotiaAssessment
+//
+//  Created by Grigor Grigoryan on 2026-06-03.
+//
+
+import Foundation
+import Observation
+
+@MainActor
+@Observable
+final class TransactionListViewModel {
+
+    // MARK: Properties
+
+
+    // MARK: Initialization
+
+    init() {
+
+    }
+
+    // MARK: Functions
+}
+
---
+++ ScotiaAssessment/Views/TransactionListView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionListView.swift
+//  ScotiaAssessment
+//
+//  Created by Grigor Grigoryan on 2026-06-03.
+//
+
+import SwiftUI
+
+struct TransactionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionListView()
+}
